253cf905dacbf817d8fcd9b7c3f6d74492e488e5
Zo25zO01Zo <40522766+Zo25zO01Zo@users.noreply.github.com>
2020-11-05 15:47:13 +0300

    Add files via upload

---
+++ firstprogram.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    cout << "ZFT" << endl;
+    return 0;
+}
---
+++ variables.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    // int , float , double
+    // 2,-2 , 3.14,-5.8
+    int x;
+    float y = 3.14;
+    cout << y << endl;
+    int r  = 5.4;
+    double d = 14.89;
+    cout << d << endl;
+    cout << r << endl;
+    x = 5;
+    cout << "x" << endl;
+    cout << x << endl;
+    return 0;
+}
